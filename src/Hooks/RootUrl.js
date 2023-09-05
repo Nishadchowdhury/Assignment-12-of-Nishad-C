b9df1b78bc68151625be2157f0731e82b838fecb
Nishadchowdhury <Nishadhj111@gmail.com>
2023-09-06 00:42:18 +0600

    RootUlr bug fix

--- src/Hooks/RootUrl.js
+++ src/Hooks/RootUrl.js
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV === "development";
-const rootUrl = !dev
+const rootUrl = dev
   ? "http://localhost:5000"
   : "https://laparts-server.onrender.com";
 export default rootUrl;
